771481c1e4938d1589d19e147fe895d28ad7fd09
SwaroopVNaik <swaroop.v.naik@gmail.com>
2025-09-26 00:09:49 +0530

    Python_Lists

---
+++ List_Methods.py
@@ -0,0 +1,89 @@
+# Python List Methods :->
+
+# Python_Lists (Array) 👇 !
+numbers = [1, 3, 2]
+
+# Methods in Python_list 👇 !
+
+# append - Function..! 
+numbers.append(4) # appending Value 4 in number[] List..!
+numbers.append(7) # appending Value 7 in number[] List..!
+numbers.append(5) # appending Value 5 in number[] List..!
+
+print("\nThe Appended Values of List -> numbers [1, 3, 2] is =>", numbers)
+
+# Sort - Function..!
+
+print("\nThe UnSorted List =>", numbers)
+numbers.sort() # It will sort the values in the List in ascending Order ( increasing order)
+
+print("\nThe Sorted List =>", numbers)
+
+# Insert - Function..!
+
+# Generic - Syntax => numbers.Insert(Index, value)
+# Index => Declare the index value where you want to insert the Value in List
+# Value => Declare The value which we want to insert In the declared Index of List 
+
+number_2 = [2, 8, 0]
+
+print("\nBefore Inserting the values in the List", number_2)
+
+number_2.insert(0, 700)
+
+print("\nAfter Inserting values in the List =>", number_2)
+
+# remove - Function..!
+
+print("\nBefore removing the values of List =>", number_2)
+
+number_2.remove(700)
+
+print("\nAfter removing the values of List =>", number_2)
+
+# reverse - function..!
+
+print("\nBefore reversing the Values in String => ", numbers)
+
+numbers.reverse()
+
+print("\nAfter Reversing the Value of the String => ", numbers)
+
+# pop - function..!
+
+# Generic - syntax => numbers.pop(Index)
+# if we dont declare any index number in the pop it pops the last index values from List
+# if we declare the pop index values of the List, it will pop the declared index value from List 
+
+print("\nBefore Popping the values from the List => ", numbers)
+
+numbers.pop(4)
+
+print("\nAfter Poping the elements from the List => ", numbers)
+
+# Accessing the Elements from the List 👇 !
+
+for number in numbers:
+    print(number)
+
+# 2 - Dimensional List In python (2-D Array)
+# 2 - row's , 3 - coloumn's 
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+
+print("\nThe 2-Dimensional List => ", matrix)
+
+# changing the values in 2-Dimensional Arrays
+
+print("\nBefore Change The values Of 2-Dimensional List => ", matrix)
+
+matrix[0][0] = 100 # changing value of 0th row index and coloumn value -> 100
+matrix[0][1] = 200 # changing value of 0th row index and 1st coloumn Index Value -> 200
+
+matrix[1][0] = 7 # changing value of 1st row index and 0th coloumn index value -> 7
+matrix[1][1] = 888 # changing value of 1st row index and 1st coloumn index value -> 888
+
+print("\nAfter Change The values Of 2-Dimensional List => ", matrix)
---
+++ Lists.py
@@ -0,0 +1,33 @@
+# In Python There are no arrays,So alternatly there are Lists...!
+# These Lists are Implemeted as Array's...!
+
+# Syntax Of Python_Lists (Array) 👇 !
+numbers = [1, 2, 3, 4, 5]
+
+# To Access Valus from Python_lists (Array) 👇 !
+
+print("\nThe 1st value in Python_List => ", numbers[0]) # Prints 1st Value..!
+print("The 2nd value in Python_List => ", numbers[1]) # Prints 2nd Value..! 
+print("The 3rd value in Python_List => ", numbers[2]) # Prints 3rd Value..! 
+print("The 4th value in Python_List => ", numbers[3]) # Prints 4th Value..!
+print("The 5th value in Python_List => ", numbers[4]) # Prints 5th Value..!
+
+# To Acess all Values from Python_Lists (Array) 👇 !
+
+print("\nThe values of Python_Lists are => ", numbers) # Prints all the Value..!
+
+# To update the Values in Python_Lists 👇 !
+
+numbers[0] = 100 # First Postion value will change..! 
+numbers[1] = 200 # Second Postion Value will change..!
+numbers[2] = 300 # Third Position Value will Change..!
+numbers[3] = 400 # Fourth Position value will change..! 
+numbers[4] = 500 # Fifth Position value will change..! 
+
+print("\nThe Changed Value of Python_List [1, 2, 3, 4, 5 ] => ", numbers)
+
+# In Python_Lists different Data_types can be stored in the list ( " Heterogenous - mixture ") 👇 !
+
+Hetrogenous_List = [1, 2, "Mahesh Sir", "Santhosh Sir", "Sangeetha Ma'am", 24.6, True]
+
+print("\nThe Python_Lists can be mixture of any data_type ", Hetrogenous_List)
