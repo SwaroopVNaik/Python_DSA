ed37f9aa6e5c0fb9452af85dbb4de79831c0eac7
SwaroopVNaik <swaroop.v.naik@gmail.com>
2025-10-12 22:41:19 +0530

    Done...!!!

--- Tuple.py
+++ Tuple.py
@@ -9,6 +9,7 @@ The Tuples are More efficient then Lists !
 Time Complexity of Tuple when Asscessing elements is O(1)
 
 """
+#------------------------------------------------------------------------------------------------------------------
 # Syntax Of Tuple ->
 # Observe Tuples use Circular Brackets while List Square Brackets
 # Tuples can be declared In two ways/methods ->
@@ -17,7 +18,7 @@ Time Complexity of Tuple when Asscessing elements is O(1)
 student_1 = ("Mahesh Arali Sir", "SDE Full stack ", "20 years Industry Experiance", 42) # -> Packing 
 
 print(student_1)
-
+#------------------------------------------------------------------------------------------------------------------
 # To print The Each Element Using Index Value ->
 print(student_1[0])
 print(student_1[1])
@@ -27,29 +28,29 @@ print(student_1[3])
 """ -> student[3] = 40 ( we cannot Add(overide) new elements in Tuple once its Declared, 
 Either once we use the declared tuple or 
 we hv to throw it and Create a new tuple but cannot add new element to the Exsiting Tuple) """
-
+#------------------------------------------------------------------------------------------------------------------
 # Method_2 of Declaring Tuples 👇 :-> 
 # packing -> Assigning all the values of tuple to one variable and printing them 
 student_2 = "Swaroop", "Aspiring SDE Full stack ", "Learning with algorithms365", 21 # -> Packing
 
 print(student_2)
-
+#-----------------------------------------------------------------------------------------------------------------
 # To print The Each Element Using Index Value ->
 print(student_2[0])
-print(student_2[1])
+print(student_2[1]) 
 print(student_2[2])
 print(student_2[3])
-
+#----------------------------------------------------------------------------------------------------------------
 # Tuples May be Nested ->
 
 country = "Bharat", student_2
 
 print(country)
-
+#-----------------------------------------------------------------------------------------------------------------
 # declaring Tuple with Single data
 Tuple_Single_Data = "India",
 print(Tuple_Single_Data)
-
+#-----------------------------------------------------------------------------------------------------------------
 # Reverse Operation Of Tuple
 student_3 = "SVN", "CSE", "4th year", 21
 # Assinging the value of tuple (student_3) to each new Variable and printing them , this un-packing 
@@ -59,7 +60,7 @@ print(name)
 print(dept)
 print(year)
 print(age)
-
+#------------------------------------------------------------------------------------------------------------------
 # Usage of Tuple :->
 # Returning Multiple Values from the Function
 # Function_Declaration 👇 :->
@@ -75,12 +76,12 @@ and Return the Reference of the Object"""
 # Function_Invokation 👇 :->
 student_marks = return_student_marks("Swaroop")
 print(student_marks)
-
+#-----------------------------------------------------------------------------------------------------------------
 # Operations in Tuple 
 
 # Returns Number of elements in Tuple
 # student_1.count() -> Takes Only One Argument
-
+#------------------------------------------------------------------------------------------------------------------
 # Craeting a Tuple 
 Colleges = "SVIT", "RVCE", "PES", "RV", "SVIT", "KLE", "SVIT"
 # This Operations (Occurance Count) is very useful when dealing with thousands of Data
@@ -88,4 +89,4 @@ Colleges = "SVIT", "RVCE", "PES", "RV", "SVIT", "KLE", "SVIT"
 count = Colleges.count("SVIT")
 print(f"The Number of Students from SVIT = {count}")
 
-#---------------------------------------------Thank-You-------------------------------------------------
+#---------------------------------------------Thank-You------------------------------------------------------------
