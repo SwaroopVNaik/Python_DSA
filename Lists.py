918dcb711f62358a83f9a6afad3f17971725ae14
SwaroopVNaik <swaroop.v.naik@gmail.com>
2025-10-20 15:48:06 +0530

    Done....!

--- Lists.py
+++ Lists.py
@@ -4,7 +4,9 @@
 # Syntax Of Python_Lists (Array) 👇 !
 numbers = [1, 2, 3, 4, 5]
 
-# To Access Valus from Python_lists (Array) 👇 !
+#--------------------------------------------------------------------------------------------------
+
+# To Access Values from Python_lists (Array) 👇 !
 
 print("\nThe 1st value in Python_List => ", numbers[0]) # Prints 1st Value..!
 print("The 2nd value in Python_List => ", numbers[1]) # Prints 2nd Value..! 
@@ -12,9 +14,13 @@ print("The 3rd value in Python_List => ", numbers[2]) # Prints 3rd Value..!
 print("The 4th value in Python_List => ", numbers[3]) # Prints 4th Value..!
 print("The 5th value in Python_List => ", numbers[4]) # Prints 5th Value..!
 
-# To Acess all Values from Python_Lists (Array) 👇 !
+#---------------------------------------------------------------------------------------------------
+
+# To Acess all the Values from Python_Lists (Array) 👇 !
 
 print("\nThe values of Python_Lists are => ", numbers) # Prints all the Value..!
+
+#---------------------------------------------------------------------------------------------------
 
 # To update the Values in Python_Lists 👇 !
 
@@ -26,8 +32,12 @@ numbers[4] = 500 # Fifth Position value will change..!
 
 print("\nThe Changed Value of Python_List [1, 2, 3, 4, 5 ] => ", numbers)
 
+#-----------------------------------------------------------------------------------------------------
+
 # In Python_Lists different Data_types can be stored in the list ( " Heterogenous - mixture ") 👇 !
 
 Hetrogenous_List = [1, 2, "Mahesh Sir", "Santhosh Sir", "Sangeetha Ma'am", 24.6, True]
 
 print("\nThe Python_Lists can be mixture of any data_type ", Hetrogenous_List)
+
+#-------------------------------------Thank-You-------------------------------------------------------
